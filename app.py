78db81b10662a50f69fcf5cdd8c923967309f1ac
Muennighoff <n.muennighoff@gmail.com>
2022-09-29 18:48:43 +0200

    v0001

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ import gradio as gr
 import requests
 import pandas as pd
 from huggingface_hub.hf_api import SpaceInfo
+from huggingface_hub import HfApi, hf_hub_download
+from huggingface_hub.repocard import metadata_load
+
 path = f"https://huggingface.co/api/spaces"
+
+
+#api = HfApi()
+#models = api.list_models(filter="mteb")
+#readme_path = hf_hub_download(models[0].modelId, filename="README.md")
+#meta = metadata_load(readme_path)
+#list(filter(lambda x: x["task"]["type"] == "Retrieval", meta["model-index"][0]["results"]))
 
 
 def get_blocks_party_spaces():
@@ -18,6 +28,27 @@ def get_blocks_party_spaces():
     df = df.sort_values(by=['likes'],ascending=False)
     return df
 
+def get_clustering(task="Clustering", metric="v_measure"):
+    api = HfApi()
+    models = api.list_models(filter="mteb")
+    df_list = []
+    for model in models:
+        readme_path = hf_hub_download(model.modelId, filename="README.md")
+        meta = metadata_load(readme_path)
+        out = list(
+            map(
+                lambda x: {x["dataset"]["name"]: list(filter(lambda x: x["type"] == metric, x["metrics"]))[0]["value"]}, 
+                filter(lambda x: x["task"]["type"] == task, meta["model-index"][0]["results"])
+            )
+        )
+        out = {k: v for d in out for k, v in d.items()}
+        out["Model"] = model.modelId
+        df_list.append(out)
+    df = pd.DataFrame(df_list)
+    # Put Model in the beginning & sort the others
+    df = df[[df.columns[-1]] + sorted(df.columns[:-1])]
+    return df
+
 block = gr.Blocks()
 
 with block:    
@@ -25,19 +56,21 @@ with block:
     with gr.Tabs():
         with gr.TabItem("Blocks Party Leaderboard"):
             with gr.Row():
-                data = gr.outputs.Dataframe(type="pandas")
+                data = gr.components.Dataframe(type="pandas")
             with gr.Row():
                 data_run = gr.Button("Refresh")
                 data_run.click(get_blocks_party_spaces, inputs=None, outputs=data)
-        with gr.TabItem("Blocks Party Leaderboard"):
+        with gr.TabItem("Clustering"):
             with gr.Row():
-                data = gr.outputs.Dataframe(type="pandas")
+                gr.Markdown("""Leaderboard for Clustering""")
+            with gr.Row():
+                data = gr.components.Dataframe(type="pandas")
             with gr.Row():
                 data_run = gr.Button("Refresh")
-                data_run.click(get_blocks_party_spaces, inputs=None, outputs=data)
+                data_run.click(get_clustering, inputs=None, outputs=data)
         with gr.TabItem("Blocks Party Leaderboard2"):
             with gr.Row():
-                data = gr.outputs.Dataframe(type="pandas")
+                data = gr.components.Dataframe(type="pandas")
             with gr.Row():
                 data_run = gr.Button("Refresh")
                 data_run.click(get_blocks_party_spaces, inputs=None, outputs=data)
